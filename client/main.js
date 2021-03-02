b76276b3022d7019afec694df4240e3b70110fce
Thykof <thykof@protonmail.ch>
2021-03-02 21:30:58 +0100

    Add code to run the split function

--- client/main.js
+++ client/main.js
@@ -20,16 +20,39 @@ const TO = '0x59dCD96E84E042aE2ff8651D53F58A1dA63Fca08';
 
 (async () => {
   // send some ether:
+  console.log("\nsend some ether");
   let result = await transfertEther(web3, ADDRESS, PRIVATE_KEY, TO, '0x6')
-  console.log(ADDRESS, result.fromBalance);
-  console.log(TO, result.toBalance);
+  console.log(`balance of ${ADDRESS} is ${result.fromBalance}`);
+  console.log(`balance of ${TO} is ${result.toBalance}`);
 
   // compile ans deploy a contract:
+  console.log("\ncompile ans deploy a contract");
   var contractData = compileContract(contractCode)
   var receipt = await deployContract(web3, contractData, ADDRESS, PRIVATE_KEY)
-  console.log(receipt);
+  let contractAddress = receipt.contractAddress
+  console.log("deployed at " + contractAddress);
 
   // run the split function:
+  console.log("\n\nrun the split function");
+  var instance;
+  var contractName = Object.keys(contractData.contracts)[0].split('.')[0]
 
-
+  instance = new web3.eth.Contract(
+      contractData.contracts[contractName + '.sol'][contractName].abi,
+      contractAddress
+  )
+  instance.methods.split("DAFT PUNK", 9).send({
+      from: ADDRESS
+  })
+  .on('receipt', receipt => {
+      let result = [
+          receipt.events["Result"].returnValues["0"],
+          receipt.events["Result"].returnValues["1"]
+      ]
+      console.log(result);
+      console.log(`["${result[0]}", "${result[1]}"]`);
+  })
+  .on('error', function(error, receipt) {
+    console.log(error, receipt);
+  })
 })()
