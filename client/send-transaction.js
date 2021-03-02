7398b0a8ade269c681d16428f6d61c863ffedce8
Thykof <thykof@protonmail.ch>
2021-03-02 21:17:18 +0100

    Improve send transaction

--- client/send-transaction.js
+++ client/send-transaction.js
@@ -1,26 +1,25 @@
-module.exports = (web3, from, privateKey, to, amount) => {
-  var rawTx = {
-    from: from,
-    to: to,
-    value: amount,
-    gas: 2000000
-  }
+module.exports = async (web3, rawTx, privateKey) => {
+  // let nonce =  await web3.eth.getTransactionCount(rawTx.from, "pending");
+  // console.log(nonce);
+  // let nonceHex = web3.utils.toHex(nonce);
+  // console.log(nonceHex);
+  //
+  // rawTx.nonce = nonceHex
 
   const signPromise = web3.eth.accounts.signTransaction(rawTx, privateKey);
 
-  signPromise.then((signedTx) => {
+  return signPromise.then((signedTx) => {
     // raw transaction string may be available in .raw or
     // .rawTransaction depending on which signTransaction
     // function was called
-    const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);  sentTx.on("receipt", receipt => {
-      // do something when receipt comes back
-      console.log(receipt)
-      web3.eth.getBalance(from).then(amount => console.log(from, amount))
-      web3.eth.getBalance(to).then(amount => console.log(to, amount))
-    });
+    const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
     sentTx.on("error", err => {
       // do something on transaction error
       console.log(err)
+    });
+    return sentTx.on("receipt", receipt => {
+      // do something when receipt comes back
+      return receipt
     });
   }).catch((err) => {
     // do something when promise fails
--- client/transfert-ether.js
+++ client/transfert-ether.js
@@ -1,3 +1,18 @@
-module.exports = (web3, from, to, amount) => {
-  
+var sendTransaction = require('./send-transaction')
+
+module.exports = async (web3, from, privateKey, to, amount) => {
+  var rawTx = {
+    from: from,
+    to: to,
+    value: amount,
+    gas: 2000000
+  }
+
+  return sendTransaction(web3, rawTx, privateKey).then(async receipt => {
+    return {
+      receipt,
+      fromBalance: await web3.eth.getBalance(from),
+      toBalance: await web3.eth.getBalance(to)
+    }
+  })
 }
