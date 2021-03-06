1c0b8881a33e9ce58ed57365e03f589a992341e5
Thykof <thykof@protonmail.ch>
2021-03-06 09:51:59 +0100

    Fetch gas price and ether price

--- src/main.js
+++ src/main.js
@@ -5,33 +5,32 @@ var deployContract = require('./deploy-contract')
 var compileContract = require('./compile-contract')
 var contractCode = require('./contract-code')
 var transfertEther = require('./transfert-ether')
-
-const GAS_PRICE = 70
+var api = require('./api')
 
 const web3 = new Web3(ganache.provider({
-    mnemonic: "can flip ocean logic case muffin expose ice truly correct brand budget",
-    gasPrice: GAS_PRICE
+    mnemonic: "can flip ocean logic case muffin expose ice truly correct brand budget"
 }));
+// web3.eth.defaultAccount = ADDRESS
 
 const ADDRESS = '0x07B6151f7ad24acf2818Eed239d3f5039E430FA7'
 const PRIVATE_KEY = '87e9b3b404d680a66d92dbd68a8b27ec8f182be0534d7206d600ae9c096b84af'
 const TO = '0x59dCD96E84E042aE2ff8651D53F58A1dA63Fca08';
 
-// web3.eth.defaultAccount = ADDRESS
-
 // account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
 
+let receipt;
+let totalGasUsed = 0;
 
 (async () => {
-  let receipt;
-  let totalGasUsed = 0;
+  let etherPrice = await api.getEtherPrice()
+  let gasPrice = await api.getGasPrice()
 
   // send some ether:
   console.log("\nSend some ether");
   let result = await transfertEther(web3, ADDRESS, PRIVATE_KEY, TO, '0x6')
   console.log(`  balance of ${ADDRESS} is ${result.fromBalance}`);
   console.log(`  balance of ${TO} is ${result.toBalance}`);
-  console.log(`  cost: $${result.receipt.gasUsed * GAS_PRICE / 1000000000 * 1580}`);
+  console.log(`  cost: $${result.receipt.gasUsed * gasPrice / 1000000000 * etherPrice}`);
 
   // compile and deploy a contract:
   console.log("\nCompile and deploy the contract");
@@ -39,7 +38,7 @@ const TO = '0x59dCD96E84E042aE2ff8651D53F58A1dA63Fca08';
   receipt = await deployContract(web3, contractData, ADDRESS, PRIVATE_KEY)
   totalGasUsed += receipt.gasUsed
   const contractAddress = receipt.contractAddress
-  console.log(`  deployed at ${contractAddress}, cost: ${receipt.gasUsed * GAS_PRICE / 1000000000 * 1580}`);
+  console.log(`  deployed at ${contractAddress}, cost: $${receipt.gasUsed * gasPrice / 1000000000 * etherPrice}`);
 
   // run the split function:
   console.log("\n\nRun the split function");
@@ -47,26 +46,26 @@ const TO = '0x59dCD96E84E042aE2ff8651D53F58A1dA63Fca08';
   const contractName = Object.keys(contractData.contracts)[0].split('.')[0]
 
   instance = new web3.eth.Contract(
-      contractData.contracts[contractName + '.sol'][contractName].abi,
-      contractAddress
+    contractData.contracts[contractName + '.sol'][contractName].abi,
+    contractAddress
   )
   instance.methods.split("DAFT PUNK", 9).send({
-      from: ADDRESS
+    from: ADDRESS
   })
   .on('receipt', receipt => {
     totalGasUsed += receipt.gasUsed
-      let result = [
-          receipt.events["Result"].returnValues["0"],
-          receipt.events["Result"].returnValues["1"]
-      ]
-      console.log('  ' + result);
-      console.log(`  ["${result[0]}", "${result[1]}"]`);
+    let result = [
+      receipt.events["Result"].returnValues["0"],
+      receipt.events["Result"].returnValues["1"]
+    ]
+    console.log('  ' + result);
+    console.log(`  ["${result[0]}", "${result[1]}"]`);
 
-      const costEther = receipt.gasUsed * GAS_PRICE / 1000000000
-      console.log(`  cost: ${costEther} ETHER, $${costEther * 1580}`);
+    const costEther = receipt.gasUsed * gasPrice / 1000000000
+    console.log(`  cost: ${costEther} ETHER, $${costEther * etherPrice}`);
 
-      const totalCostEther = totalGasUsed * GAS_PRICE / 1000000000
-      console.log(`\n\nTotal Cost: ${totalCostEther} ETHER, $${totalCostEther * 1580}`);
+    const totalCostEther = totalGasUsed * gasPrice / 1000000000
+    console.log(`\n\nTotal Cost: ${totalCostEther} ETHER, $${totalCostEther * etherPrice}`);
   })
   .on('error', function(error, receipt) {
     console.log(error, receipt);
